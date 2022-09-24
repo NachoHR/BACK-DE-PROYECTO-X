43ede7cd88ef9f89f79be692064d4ad3b6b861eb
NachoHR <AXLNACHO@GMAIL.COM>
2022-09-24 18:54:40 -0500

    back

---
+++ src/app.controller.ts
@@ -0,0 +1,48 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  
+  @Get('/productos')
+  mostrarproductos(): any {
+    return [
+      {
+        codigo: 't01',
+        nombre: 'cañaso',
+        costo: '100',
+        imagen: '',
+      },
+      {
+        codigo: 't02',
+        nombre: 'vino',
+        costo: '30',
+        imagen: '',
+      },
+      {
+        codigo: 't03',
+        nombre: 'pisco',
+        costo: '8',
+        imagen: '',
+      },
+      {
+        codigo: 't04',
+        nombre: 'wiscky',
+        costo: '50',
+        imagen: '',
+      },
+      {
+        codigo: 't05',
+        nombre: 'ron',
+        costo: '27',
+        imagen: '',
+      },
+    ];
+  }
+}
